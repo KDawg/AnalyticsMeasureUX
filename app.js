bd8bc3f67901d276cd0f0cdbe53c3a2c5d771cc0
Ken Tabor <katworks@gmail.com>
2016-04-05 22:24:32 -0500

    clear out and set to first good working order

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-  console.log('booga booga booga!');
 
 
-  $('#color-choices').on('change', function() {
-    console.log('ticked my drop down!');
-  });
-
-
-  $('#btn-accept').on('click', function() {
-    console.log('clicked me!');
-  })
 });
